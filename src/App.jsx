6b54b8f68d7a29be8e2976fdf679e9ed462451d7
nateachino <natealleyne88@gmail.com>
2023-02-12 21:37:15 -0500

    Added react-player

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
 import { createFFmpeg } from '@ffmpeg/ffmpeg'
+import ReactPlayer from 'react-player';
 
 function App() {
+  const [wait, setWait] = useState(false)
   const [video, setVideo] = useState('');
 
   const ffmpg = createFFmpeg()
@@ -16,12 +18,18 @@ function App() {
       <form>
         <h1>Upload Video</h1>
         <input type="file" onChange={(e)=>{
+          setVideo(e.target.files[0])
           console.log(e.target.files[0])
+          console.log(e)
         }}></input>
         <button type="submit" onClick={(e)=>{
           e.preventDefault()
+          setWait(true)
+          transcode()
         }}>Submit</button>
       </form>
+
+        {wait ? <ReactPlayer url={URL.createObjectURL(video)}/> : null}
     </div>
   )
 }
