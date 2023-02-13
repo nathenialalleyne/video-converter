60e2991c2a2a376c94abfdf3d42f04d6a7bdf3cc
nateachino <natealleyne88@gmail.com>
2023-02-13 13:08:47 -0500

    Added conversion for AVI format

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
-import { useEffect, useState } from 'react'
-import { createFFmpeg } from '@ffmpeg/ffmpeg'
+import { useState } from 'react'
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg'
 import ReactPlayer from 'react-player';
 
 function App() {
-  const [wait, setWait] = useState(false)
-  const [video, setVideo] = useState();
+    const [video, setVideo] = useState();
+    const [data, setData] = useState()
 
-  const ffmpg = createFFmpeg()
+  const ffmpg = createFFmpeg({log: true})
 
   const transcode = async () =>{
     await ffmpg.load()
-    await ffmpg.FS('readFile', video)
-  }
+    // await ffmpg.FS('writeFile', video, await fetchFile(video))
+    // console.log("validated")
+    await ffmpg.FS('writeFile', 'file.avi', await fetchFile(video))
+    await ffmpg.run('-i', "file.avi", "output.mp4")
+    const videoData = await ffmpg.FS('readFile', 'output.mp4')
+    setData(URL.createObjectURL(new Blob([videoData.buffer], {type: 'video/mp4'})))
 
-  useEffect(()=>{
-    console.log(video)
-  })
+  }
 
   return (
     <div>
@@ -23,19 +25,14 @@ function App() {
         <h1>Upload Video</h1>
         <input type="file" onChange={(e)=>{
           setVideo(e.target.files[0])
-          console.log(e.target.files[0])
-          if (video){
-            console.log("video found")
-          }
         }}></input>
         <button type="submit" onClick={(e)=>{
           e.preventDefault()
-          setWait(true)
           transcode()
         }}>Submit</button>
       </form>
 
-        <video src={video} width="500" height="500"></video>
+        {data ? <ReactPlayer url={data}/> : null}
     </div>
   )
 }
