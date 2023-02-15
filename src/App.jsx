54631687b192e0a8e54eccf9d0400040f4ec2aaf
nateachino <natealleyne88@gmail.com>
2023-02-14 22:02:57 -0500

    Added styling and changed submit button to appear only when file input

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
 import ReactPlayer from "react-player";
 
@@ -6,7 +6,9 @@ function App() {
   const [video, setVideo] = useState();
   const [data, setData] = useState();
   const [videoName, setVideoName] = useState();
-  const [error, setError] = useState(false);
+  const [stage, setStage] = useState("Upload Video");
+
+  const ref = useRef();
 
   const ffmpg = createFFmpeg({ log: true });
 
@@ -21,8 +23,11 @@ function App() {
       setData(
         URL.createObjectURL(new Blob([videoData.buffer], { type: "video/mp4" }))
       );
+      setStage("Finished");
+      ref.current.style.visibility = "visible";
     } catch (err) {
-      setError(true);
+      setStage("Error converting, please try again.");
+      ref.current.style.visibility = "visible";
     }
   };
 
@@ -31,17 +36,10 @@ function App() {
   });
 
   return (
-    <div className="w-full">
-      <form>
-        {video ? (
-          <ReactPlayer
-            url={URL.createObjectURL(new Blob([video], { type: video.type }))}
-            muted={true}
-            playing={true}
-            loop={true}
-          />
-        ) : null}
-        <h1>Upload Video</h1>
+    <div className="w-screen h-screen flex justify-center items-center bg-slate-400 flex-col">
+      <h1 className="text-5xl pb-4">Convert video to MP4!</h1>
+      <form className="flex flex-col grow-0 justify-center items-center bg-slate-300 p-4 ">
+        <h2>{stage}</h2>
         <input
           type="file"
           onChange={(e) => {
@@ -49,19 +47,28 @@ function App() {
             setVideoName(e.target.files[0].name);
           }}
         ></input>
-        <button
-          type="submit"
-          onClick={(e) => {
-            e.preventDefault();
-            transcode();
-          }}
-        >
-          Submit
-        </button>
+        {video ? (
+          <button
+            ref={ref}
+            className="border p-2 bg-blue-100 rounded w-fit"
+            type="submit"
+            onClick={(e) => {
+              e.preventDefault();
+              setStage("Processing Video... Please wait");
+              ref.current.style.visibility = "hidden";
+              video ? transcode() : null;
+            }}
+          >
+            Submit
+          </button>
+        ) : null}
       </form>
 
       {data ? (
-        <ReactPlayer url={data} muted={true} playing={true} loop={true} />
+        <div className="flex flex-col justify-center items-center">
+          <h2 className="text-2xl">Right Click to Download</h2>
+          <ReactPlayer url={data} muted={true} playing={true} loop={true} />
+        </div>
       ) : null}
     </div>
   );
